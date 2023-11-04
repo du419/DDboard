b6a05b4b48555dbd45f27519a09cc79b22777f57
kbs <dhlrpdls438@gmail.com>
2023-11-04 22:09:50 +0900

    주석

---
+++ src/main/java/com/ddBoard/bord/BordApplication.java
@@ -0,0 +1,13 @@
+package com.ddBoard.bord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddBoard/bord/BordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddBoard.bord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
